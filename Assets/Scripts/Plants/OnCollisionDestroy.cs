3240565a47e17355334c009254b5d2948c42a990
Lorena P <lorena.1997.lore@gmail.com>
2024-10-27 12:01:33 +0100

    Plantas infinitas (Vuelven a crecer)

--- Assets/Scripts/Plants/OnCollisionDestroy.cs
+++ Assets/Scripts/Plants/OnCollisionDestroy.cs
@@ -7,10 +7,17 @@ using UnityEngine;
 
 public class OnCollisionDestroy : MonoBehaviour
 {
-    private GameObject player; 
+    private GameObject player;
+    public int tiempoDeEsperaParaActivarObjeto = 4;
+    [SerializeField] Collider thisCollider;
+    [SerializeField] Renderer thisRenderer;
+    
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player"); 
+        thisCollider = GetComponent<Collider>();
+        thisRenderer = GetComponent<Renderer>();    
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -20,16 +27,37 @@ public class OnCollisionDestroy : MonoBehaviour
             if (gameObject.tag == "Flor")
                 player.GetComponent<Inventory>().flor++;
 
+
             if (gameObject.tag == "Mush")
                 player.GetComponent<Inventory>().mush++;
+
 
             if (gameObject.tag == "Hierba")
                 player.GetComponent<Inventory>().hierba++;
 
-            Destroy(gameObject, 1);
+            DesactivarObjeto();
 
+          
         }
     }
 
-    
+    IEnumerator ActivarObjeto()
+    {
+        print("Activar Objeto");
+        yield return new WaitForSeconds(tiempoDeEsperaParaActivarObjeto);
+        thisCollider.enabled = true;
+        thisRenderer.enabled = true;
+        
+    }
+    private void DesactivarObjeto()
+    {
+        thisCollider.enabled = false;
+        thisRenderer.enabled = false;
+
+       StartCoroutine(ActivarObjeto());
+    }
+
+
+
+
 }
